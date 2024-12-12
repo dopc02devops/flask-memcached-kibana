84912a1691a3f33a2f7952a73b2f9c00e54730b1
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2024-12-12 04:26:55 +0000

    Added vpc templates

--- Terraform-Templates/vpc/vpc.tf
+++ Terraform-Templates/vpc/vpc.tf
@@ -1,19 +1,162 @@
+
+########################################
+# Sets up the AWS provider for Terraform
+# to interact with the AWS environment
+########################################
 provider "aws" {
   region = "eu-west-2"
 }
 
-provider "aws" "assume_role" {
+provider "aws" {
+  alias  = "assumed"
   region = "eu-west-2"
   assume_role {
-    role_arn = var.role_arn # Use environment variable for Role ARN
+    role_arn = env.role_arn
   }
 }
 
-variable "role_arn" {
-  description = "IAM Role ARN for assuming a role"
-  type        = string
-  default     = "${env.ROLE_ARN}" # Role ARN is provided as an environment variable
+########################################
+# Creates a Virtual Private Cloud (VPC)
+# with a specified CIDR block
+########################################
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.vpc_name
+  }
 }
+
+########################################
+# Creates an Internet Gateway (IGW) to
+# enable outbound internet access for
+# resources within the VPC
+########################################
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+########################################
+# Creates public/private subnets in the
+# VPC where instances have/do not internet
+# access
+########################################
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.public_subnet_name}-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 2)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.private_subnet_name}-${count.index}"
+  }
+}
+
+########################################
+# Manages routing for public subnets to
+# direct internet-bound traffic via the IGW
+########################################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.public_route_table_name
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table-${count.index + 1}"
+  }
+}
+########################################
+# Defines a route in the public route table
+# to allow traffic to the internet.
+########################################
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "private" {
+  count                  = 2
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+}
+
+########################################
+# Links the public route table to public
+# subnets
+########################################
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+
+########################################
+# Creates Network Address Translation (NAT)
+# gateways for private subnets to access
+# the internet securely
+########################################
+resource "aws_nat_gateway" "nat" {
+  count        = 2
+  allocation_id = aws_eip.nat[count.index].id # Elastic IPs associated with the NAT gateways
+  subnet_id     = aws_subnet.public[count.index].id # Places NAT gateways in the public subnets
+
+  tags = {
+    Name = "nat-gateway-${count.index + 1}"
+  }
+}
+
+########################################
+# Allocates Elastic IPs for use with the
+# NAT gateways
+########################################
+resource "aws_eip" "nat" {
+  count = 2
+  domain = "vpc"
+}
+
+########################################
+# Fetches the list of available availability
+# zones in the specified region
+########################################
+
+data "aws_availability_zones" "available" {}
+
+#####################
+# Variables
+#####################
 
 variable "vpc_name" {
   description = "Name for the VPC"
@@ -51,141 +194,48 @@ variable "private_route_table_name" {
   default     = "private-route-table"
 }
 
-variable "nat_gateway_name" {
-  description = "Name for the NAT Gateway"
-  type        = string
-  default     = "nat-gateway"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = var.vpc_name
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = var.igw_name
-  }
-}
-
-resource "aws_subnet" "public" {
-  count                   = 2 # Adjust for the number of availability zones
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "${var.public_subnet_name}-${count.index}"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count             = 2 # Adjust for the number of availability zones
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 2)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "${var.private_subnet_name}-${count.index}"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = var.public_route_table_name
-  }
-}
-
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id # NAT gateway is in the first public subnet
-
-  tags = {
-    Name = var.nat_gateway_name
-  }
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = var.private_route_table_name
-  }
-}
-
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
-
-resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
-
-data "aws_availability_zones" "available" {}
+#########################
+# Outputs
+#########################
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
   description = "The ID of the VPC."
 }
 
 output "vpc_name" {
-  value = aws_vpc.main.tags["Name"]
+  value       = aws_vpc.main.tags["Name"]
   description = "The name of the VPC."
 }
 
 output "public_subnets" {
-  value = aws_subnet.public[*].id
+  value       = aws_subnet.public[*].id
   description = "IDs of public subnets."
 }
 
 output "private_subnets" {
-  value = aws_subnet.private[*].id
+  value       = aws_subnet.private[*].id
   description = "IDs of private subnets."
 }
 
 output "internet_gateway_id" {
-  value = aws_internet_gateway.igw.id
+  value       = aws_internet_gateway.igw.id
   description = "The ID of the Internet Gateway."
 }
 
 output "nat_gateway_id" {
-  value = aws_nat_gateway.nat.id
-  description = "The ID of the NAT Gateway."
+  value       = aws_nat_gateway.nat[*].id
 }
 
-
+output "private_route_table_ids" {
+  value       = aws_route_table.private[*].id
+  description = "The IDs of the private route tables."
+}
 
 
 # export ROLE_ARN=arn:aws:iam::123456789012:role/example-role
 # echo $ROLE_ARN
 # terraform init
+# terraform plan
 # terraform apply
+# terraform destroy
