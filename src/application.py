33f5ff7266e2c88a85ead5f6ff648b9a8c1e61ac
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2025-01-03 03:21:41 +0000

    first commit

--- src/application.py
+++ src/application.py
@@ -155,7 +155,6 @@ class MemcachedApp(ConnectionManager):
     def run(self):
         self.app.run(host='0.0.0.0', port=8095, debug=True)
 
-
 if __name__ == '__main__':
     app_instance = MemcachedApp()
     app_instance.run()
